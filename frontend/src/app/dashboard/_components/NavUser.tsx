7d494301d592304436e04c7deddd5e9177705444
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2025-09-19 20:01:25 +0600

    minor changes

--- frontend/src/app/dashboard/_components/NavUser.tsx
+++ frontend/src/app/dashboard/_components/NavUser.tsx
@@ -63,7 +63,7 @@ export function NavUser({
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium">{user.name} </span>
                   <span className="text-muted-foreground truncate text-xs">{user.phone}</span>
                 </div>
               </div>
